7491b84b5ee11a9f06d287ce18e84a35551dd18f
faiface <faiface2202@gmail.com>
2024-07-22 20:01:27 +0200

    docs: add main crate intro

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! Session types are used to model communication protocols for message-passing
+//! concurrent systems and type-check their implementations, guaranteeing:
+//!
+//! - **Protocol adherence** -- Expectations delivered, obligations fulfilled.
+//! - **Deadlock freedom** -- The two ends of each exchange are always isolated from each other.
+//!
+//! This not only prevents a range of concurrency-related bugs, but also helps
+//! with designing concurrent protocols. An unsound protocol not covering all of its
+//! inherent paths of execution will be exposed while attempting to implement it.
+//!
+//! Additionally, complex concurrent systems can be modelled and implemented
+//! with confidence, as any type-checked implementation is guaranteed to adhere
+//! to its protocol, and avoid any deadlocks.
+//!
+//! _The particular flavor of session types presented here is a full implementation
+//! of propositional linear logic. No knowledge of linear logic is required to use
+//! this library. Nevertheless, using these session types can help a great deal
+//! in understanding linear logic, if one so desires._
+
 pub mod exchange;
 pub mod pool;
 pub mod queue;
