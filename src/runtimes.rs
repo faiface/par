8ef6a6f0f373d62d9c5adf58484240b06150b26a
faiface <faiface2202@gmail.com>
2024-07-26 00:12:20 +0200

    runtimes: swap S and S::Dual in fork

--- src/runtimes.rs
+++ src/runtimes.rs
@@ -1,33 +1,33 @@
 pub mod tokio {
     use futures::Future;
 
-    use crate::{Dual, Session};
+    use crate::Session;
 
-    pub fn fork<S: Session, F>(f: impl FnOnce(S) -> F) -> Dual<S>
+    pub fn fork<S: Session, F>(f: impl FnOnce(S::Dual) -> F) -> S
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        Dual::<S>::fork_sync(|session| drop(tokio::spawn(f(session))))
+        S::fork_sync(|session| drop(tokio::spawn(f(session))))
     }
 }
 
 pub mod spawn {
     use futures::{task::SpawnExt, Future};
 
-    use crate::{Dual, Session};
+    use crate::Session;
 
     pub trait Fork {
-        fn fork<S: Session, F>(&self, f: impl FnOnce(S) -> F) -> Dual<S>
+        fn fork<S: Session, F>(&self, f: impl FnOnce(S::Dual) -> F) -> S
         where
             F: Future<Output = ()> + Send + 'static;
     }
 
     impl<Spawn: futures::task::Spawn> Fork for Spawn {
-        fn fork<S: Session, F>(&self, f: impl FnOnce(S) -> F) -> Dual<S>
+        fn fork<S: Session, F>(&self, f: impl FnOnce(S::Dual) -> F) -> S
         where
             F: Future<Output = ()> + Send + 'static,
         {
-            Dual::<S>::fork_sync(|session| self.spawn(f(session)).ok().expect("spawn failed"))
+            S::fork_sync(|session| self.spawn(f(session)).ok().expect("spawn failed"))
         }
     }
 }
@@ -35,20 +35,20 @@ pub mod spawn {
 pub mod local_spawn {
     use futures::{task::LocalSpawnExt, Future};
 
-    use crate::{Dual, Session};
+    use crate::Session;
 
     pub trait Fork {
-        fn fork<S: Session, F>(&self, f: impl FnOnce(S) -> F) -> Dual<S>
+        fn fork<S: Session, F>(&self, f: impl FnOnce(S::Dual) -> F) -> S
         where
             F: Future<Output = ()> + Send + 'static;
     }
 
     impl<Spawn: futures::task::LocalSpawn> Fork for Spawn {
-        fn fork<S: crate::Session, F>(&self, f: impl FnOnce(S) -> F) -> crate::Dual<S>
+        fn fork<S: Session, F>(&self, f: impl FnOnce(S::Dual) -> F) -> S
         where
             F: futures::Future<Output = ()> + Send + 'static,
         {
-            Dual::<S>::fork_sync(|session| self.spawn_local(f(session)).ok().expect("spawn failed"))
+            S::fork_sync(|session| self.spawn_local(f(session)).ok().expect("spawn failed"))
         }
     }
 }
