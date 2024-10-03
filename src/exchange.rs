a7c7a8179db4c1632fb222002de2bdf4715a5d49
faiface <faiface2202@gmail.com>
2024-10-03 14:54:39 +0200

    exchange: finish docs

--- src/exchange.rs
+++ src/exchange.rs
@@ -1,31 +1,22 @@
-//! Sessions that first exchange a single value (possibly also a session) between
-//! their two counterparts, then proceed according to another specified session. The
-//! two sides, receiving and sending, are [`Recv`] and [`Send`] respectively.
+//! Sessions that exchange a single value (possibly another session), then proceed according to
+//! a continuation session. The two sides, receiving and sending, are [`Recv`] and [`Send`],
+//! respectively.
 //!
-//! A session exchanging a value of type `T` and continuing according to a session
-//! `S` will be operated via two handles: [`Recv<T, S>`] in one coroutine, cooperating
-//! with a [`Send<T, Dual<S>>`] in another.
-//!
-//! After exchanging a `T`, the two counterparts obtain handles for `S` and
-//! [`Dual<S>`](super::Dual) respectively, continuing their cooperation according
-//! to the session `S`.
-//!
-//! # Blocking and `.await`
+//! ## Blocking and `.await`
 //!
 //! Sending is always non-blocking. Only receiving needs to block (asynchronously) until
-//! a value has been sent from the other side. This means it's possible to perform multiple
-//! sends without waiting for the recipients to be ready.
+//! a value is supplied by the other side. It's possible to perform multiple sends without waiting
+//! for the recipients to be ready.
 //!
-//! # Correspondence to linear logic
+//! ## Correspondence to linear logic
 //!
-//! [`Recv`] and [`Send`] directly correspond to the multiplicative connectives of
-//! linear logic. Specifically:
+//! [`Recv`] and [`Send`] directly correspond to the multiplicative connectives of linear logic.
 //!
 //! - `Recv<A, B>` is **A ⊗ B**
 //! - `Send<A, B>` is **A<sup>⊥</sup> ⅋ B**
 //!
-//! And, their unary versions composed with enums correspond to the additive connectives
-//! of linear logic. Using the enum `Result`, we have:
+//! Their unary versions composed with enums correspond to the additive connectives of linear logic.
+//! For example, with `Result`:
 //!
 //! - `Recv<Result<A, B>>` is **A ⊕ B**
 //! - `Send<Result<A, B>>` is **A<sup>⊥</sup> & B<sup>⊥</sup>**
@@ -34,11 +25,46 @@ use super::Session;
 use futures::channel::oneshot;
 use std::marker;
 
+/// A session that supplies a value of type `T`, then proceeds according to `S`. Its dual
+/// is [`Send<T, Dual<S>>`].
+///
+/// Use [`recv`](Self::recv) to obtain the supplied value along with the continuation `S`.
+/// If the continuation is `()` (the empty session), use [`recv1`](Self::recv1) to obtain `T`
+/// and discard the continuation.
+///
+/// ## Correspondence to linear logic
+///
+/// - `Recv<A, B>` is **A ⊗ B**
+/// - `Recv<Result<A, B>>` is **A ⊕ B**
 #[must_use]
 pub struct Recv<T, S: Session = ()> {
     rx: oneshot::Receiver<Exchange<T, S>>,
 }
 
+/// A session that consumes a value of type `T`, then proceeds according to `S`. Its dual
+/// is [`Recv<T, Dual<S>>`].
+///
+/// Use [`send`](Self::send) to supply the requested value and obtain the continuation `S`.
+/// If the continuation is `()` (the empty session), use [`send1`](Self::send1) to discard the
+/// continuation.
+///
+/// ```
+/// let sequence: Send<i64, Send<i64>>;
+/// sequence.send(5).send1(7);
+/// ```
+///
+/// If `T` is an `enum` holding other session types (and `S` is `()`), use [`choose`](Self::choose) to
+/// pick a branch and directly handle the [dual](crate::Session::Dual) of the supplied session.
+///
+/// ```
+/// let branching: Send<Result<Recv<i64>, ()>>;
+/// branching.choose(Ok).send1(7);
+/// ```
+///
+/// ## Correspondence to linear logic
+///
+/// - `Send<A, B>` is **A<sup>⊥</sup> ⅋ B**
+/// - `Send<Result<A, B>>` is **A<sup>⊥</sup> & B<sup>⊥</sup>**
 #[must_use]
 pub struct Send<T, S: Session = ()> {
     tx: oneshot::Sender<Exchange<T, S::Dual>>,
@@ -98,6 +124,7 @@ impl<T, S: Session> Recv<T, S>
 where
     T: marker::Send + 'static,
 {
+    /// Wait to obtain a value of type `T` along with the continuation `S`.
     #[must_use]
     pub async fn recv(mut self) -> (T, S) {
         loop {
@@ -113,6 +140,7 @@ impl<T> Recv<T, ()>
 where
     T: marker::Send + 'static,
 {
+    /// Wait to obtain a value of type `T`, and discard the empty continuation.
     pub async fn recv1(self) -> T {
         self.recv().await.0
     }
@@ -122,6 +150,7 @@ impl<T, S: Session> Send<T, S>
 where
     T: marker::Send + 'static,
 {
+    /// Supply a value of type `T` and obtain the continuation `S`.
     #[must_use]
     pub fn send(self, value: T) -> S {
         S::fork_sync(|dual| {
@@ -137,10 +166,18 @@ impl<T> Send<T, ()>
 where
     T: marker::Send + 'static,
 {
+    /// Supply a value of type `T`, and discard the empty continuation.
     pub fn send1(self, value: T) {
         self.send(value)
     }
 
+    /// If the expected value is an `enum` holding sessions, choose a branch from the `enum`'s variants,
+    /// and directly obtain the [dual](crate::Session::Dual) of the supplied session.
+    ///
+    /// ```
+    /// let branching: Send<Result<Recv<i64>, ()>>;
+    /// branching.choose(Ok).send1(7);
+    /// ```
     #[must_use]
     pub fn choose<S: Session>(self, choice: impl FnOnce(S) -> T) -> S::Dual {
         S::Dual::fork_sync(|session| self.send1(choice(session)))
@@ -148,6 +185,12 @@ where
 }
 
 impl<S: Session> Send<S, ()> {
+    /// If the expected value is a session, supply it and directly obtain its [dual](crate::Session::Dual).
+    ///
+    /// ```
+    /// let needs_session: Send<Recv<i64>>;
+    /// needs_session.handle().send1(7);
+    /// ```
     #[must_use]
     pub fn handle(self) -> S::Dual {
         S::Dual::fork_sync(|session| self.send1(session))
