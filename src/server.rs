4d5f21bcdb3668aee5db8793b6d9f4a856f882a2
faiface <faiface2202@gmail.com>
2024-10-04 17:45:47 +0200

    server: write part of module docs

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,50 @@
+//! Handle a dynamic number of clients with connection and interaction protocols
+//! specified by session types. Start a [`Server`], initiate the connection protocol
+//! via a [`Proxy`], and obtain an active [`Connection`] to resume the interaction
+//! later.
+//!
+//! The communication structure of a server is composed of three parts:
+//! 
+//! - [`Server`] is the central manager of the clients. Use [`poll`](Server::poll) to handle
+//!   new connections (coming from [proxies](Proxy)), as well as resumptions of existing ones.
+//!   Use [`suspend`](Server::suspend) to create [`Connection`] handles for later resumption.
+//! - [`Proxy`] is a handle for initiating new connections. It can be dropped, or duplicated
+//!   using (atypical) [`clone`](Proxy::clone). Use [`connect`](Proxy::connect) to initiate
+//!   a connection protocol.
+//! - [`Connection`] is an active, suspended connection to the server. It must not
+//!   be dropped, disconnecting from a server should only be done in a controlled way specified
+//!   in its interaction (resumption) protocol. Use [`resume`](Connection::resume) to enter the
+//!   server's event loop.
+//!
+//! To maintain the guarantees of protocol adherance and deadlock freedom, **none two of those
+//! parts can ever be in the same scope,** see each other. That is including two parts of the same
+//! type! (One [`Proxy`] cannot see another [`Proxy`].)
+//!
+//! The signatures of the methods maintain the scoping. For example, [`Proxy::clone`] does not
+//! return a new [`Proxy`], but passes it to a closure.
+//!
+//! ```
+//! let proxy1: Proxy<Connect>;
+//! proxy1.clone(|proxy2| {
+//!     // I see proxy2, but not proxy1
+//! })
+//! // I see proxy1, but not proxy2
+//! ```
+//!
+//! **Note:** If the scoping was not maintained, deadlocks could occur. For example, having a [`Server`] and
+//! a [`Connection`] in the same scope, one could call [`resume`](Connection::resume) and get stuck
+//! on the resumption because the server would not have a chance to call [`poll`](Server::poll).
+//!
+//! ## Usage
+//!
+//! TODO
+//!
+//! ## Correspondence to linear logic
+//!
+//! [Proxies](Proxy) are an implementation of coexponentials. While not a standard part of linear logic,
+//! they come from a nice paper from 2021,
+//! ['Client-server sessions in linear logic.'](https://dl.acm.org/doi/10.1145/3473567)
+
 use super::Session;
 use futures::{channel::mpsc, StreamExt};
 use std::collections::HashMap;
@@ -39,6 +86,9 @@ where
 }
 
 impl<C: Session, R: Session, D> Server<C, R, D> {
+    /// Creates a new [`Server`] and passes a [`Proxy`] to it to the provided closure. Use the proxy
+    /// to initiate connections to the server and the returned server to [poll](Self::poll)
+    /// [events](Event) of initiating and resuming connections.
     #[must_use]
     pub fn start(f: impl FnOnce(Proxy<C::Dual>)) -> Self {
         let (tx, rx) = mpsc::channel(0);
