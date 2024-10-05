cec4d1b92ded89aa24bf2e40f2bb5d750b993f86
faiface <faiface2202@gmail.com>
2024-10-05 12:02:05 +0200

    server: complete module docs

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,6 @@
-//! Handle a dynamic number of clients with connection and interaction protocols
-//! specified by session types. Start a [`Server`], initiate the connection protocol
-//! via a [`Proxy`], and obtain an active [`Connection`] to resume the interaction
-//! later.
+//! Handle a dynamic number of clients with interaction protocols specified by session types.
+//! Start a [`Server`], initiate the connection protocol via a [`Proxy`], and obtain an active
+//! [`Connection`] to resume the interaction later.
 //!
 //! The communication structure of a server is composed of three parts:
 //! 
@@ -16,8 +15,8 @@
 //!   in its interaction (resumption) protocol. Use [`resume`](Connection::resume) to enter the
 //!   server's event loop.
 //!
-//! To maintain the guarantees of protocol adherance and deadlock freedom, **none two of those
-//! parts can ever be in the same scope,** see each other. That is including two parts of the same
+//! To maintain the guarantees of protocol adherence and deadlock freedom, **none two of those
+//! parts can ever be in the same scope** (see each other). That is including two parts of the same
 //! type! (One [`Proxy`] cannot see another [`Proxy`].)
 //!
 //! The signatures of the methods maintain the scoping. For example, [`Proxy::clone`] does not
@@ -35,10 +34,6 @@
 //! a [`Connection`] in the same scope, one could call [`resume`](Connection::resume) and get stuck
 //! on the resumption because the server would not have a chance to call [`poll`](Server::poll).
 //!
-//! ## Usage
-//!
-//! TODO
-//!
 //! ## Correspondence to linear logic
 //!
 //! [Proxies](Proxy) are an implementation of coexponentials. While not a standard part of linear logic,
@@ -49,6 +44,28 @@ use super::Session;
 use futures::{channel::mpsc, StreamExt};
 use std::collections::HashMap;
 
+/// Listens to connection initiatioins (from a [`Proxy`]) and resumptions (from a [`Connection`]) and
+/// maintains local data for each active connection. Use [`suspend`](Self::suspend) to create or maintain
+/// active an connection.
+///
+/// The three generic parameters are as follows:
+///
+/// - **`Connect`** -- The connection initiation protocol. Involves any communication needed for
+///   establishing a valid connection, such as login details. A successful initiation should result in
+///   the client receiving a [`Connection`] at the end of the protocol; the server can be used to create
+///   it by calling [`suspend`](Self::suspend).
+///
+/// - **`Resume`** -- The connection resumption protocol. When a client obtains a [`Connection`], it will
+///   use it to resume the interaction using this protocol. Any suspended [`Connection`] will be resumed
+///   and enter the server's event loop along with its local connection data. The protocol may result
+///   in the client obtaining a [`Connection`] again -- the connection is kept active -- or not, in which
+///   case the connection is ended.
+///
+/// - **`ConnectionData`** -- Local data associated with each active connection. The data can be used to
+///   identify the client, as well as carry state that's updated with each suspension. Only the server side
+///   can access this data directly.
+///
+/// Must not be dropped.
 #[must_use]
 pub struct Server<Connect, Resume, ConnectionData>
 where
@@ -61,36 +78,52 @@ where
     next_id: usize,
 }
 
+/// A handle for initiating new connections with the corresponding [server](Server). Use
+/// [`connect`](Self::connect) to start the connection initiation protocol.
+///
+/// Can be duplicated with [`Proxy::clone`], or dropped at will.
 pub struct Proxy<Connect: Session> {
     connect: Box<dyn SenderFn<Connect::Dual>>,
 }
 
+/// A client's handle to an active suspended connection. Use [`resume`](Self::resume) to enter the
+/// server's event loop and continue interaction.
+///
+/// Must not be dropped. Specify the resumption protocol to handle disconnecting.
 #[must_use]
 pub struct Connection<Resume: Session> {
     resume: Box<dyn FnOnce(Resume::Dual) + Send>,
 }
 
+/// Connection initiation or resumption event.
 #[must_use]
-pub enum Event<Connect, Resume, ConnectionData = ()>
+pub enum Event<Connect, Resume, ConnectionData>
 where
     Connect: Session,
     Resume: Session,
 {
+    /// Connection initiation event.
     Connect {
+        /// Server's end-point of the initiation protocol.
         session: Connect,
     },
+
+    /// Connection resumption event.
     Resume {
+        /// Server's end-point of the resumption protocol.
         session: Resume,
+
+        /// Local connection data previously assigned with [`suspend`](Server::suspend).
         data: ConnectionData,
     },
 }
 
-impl<C: Session, R: Session, D> Server<C, R, D> {
-    /// Creates a new [`Server`] and passes a [`Proxy`] to it to the provided closure. Use the proxy
-    /// to initiate connections to the server and the returned server to [poll](Self::poll)
+impl<Connect: Session, Resume: Session, ConnectionData> Server<Connect, Resume, ConnectionData> {
+    /// Creates a new [`Server`] and passes a [`Proxy`] to it to the provided closure. Use the [`Proxy`]
+    /// to initiate connections to the server and use the returned [`Server`]` to [poll](Self::poll)
     /// [events](Event) of initiating and resuming connections.
     #[must_use]
-    pub fn start(f: impl FnOnce(Proxy<C::Dual>)) -> Self {
+    pub fn start(f: impl FnOnce(Proxy<Connect::Dual>)) -> Self {
         let (tx, rx) = mpsc::channel(0);
 
         let mut proxy_sender = Sender(tx.clone());
@@ -111,7 +144,10 @@ impl<C: Session, R: Session, D> Server<C, R, D> {
         }
     }
 
-    pub fn suspend(&mut self, data: D, f: impl FnOnce(Connection<R::Dual>)) {
+    /// Creates or maintains an active connection and associates local data to it. The data will later
+    /// come back with a [resumption event](Event::Resume). Use this method to pass a [`Connection`] to
+    /// a client during the initiation and resumption protocols.
+    pub fn suspend(&mut self, data: ConnectionData, f: impl FnOnce(Connection<Resume::Dual>)) {
         let sender = self.sender.clone();
         let id = self.next_id;
         self.next_id += 1;
@@ -127,8 +163,11 @@ impl<C: Session, R: Session, D> Server<C, R, D> {
         })
     }
 
+    /// Waits for the next connection initiation (from a [`Proxy`]) or resumption (from a [`Connection`]).
+    /// Returns the corresponding event along with a new [`Server`] handle. In case no more [proxies](Proxy)
+    /// or [connections](Connection) exist, [`None`] is returned and the [`Server`] is dropped.
     #[must_use]
-    pub async fn poll(mut self) -> Option<(Self, Event<C, R, D>)> {
+    pub async fn poll(mut self) -> Option<(Self, Event<Connect, Resume, ConnectionData>)> {
         drop(self.sender);
         match self.receiver.0.next().await {
             Some((sender, trans)) => {
@@ -147,23 +186,29 @@ impl<C: Session, R: Session, D> Server<C, R, D> {
     }
 }
 
-impl<C: Session> Proxy<C> {
+impl<Connect: Session> Proxy<Connect> {
+    /// Duplicates the [proxy](Proxy) into another scope. No two [proxies](Proxy) can ever see each
+    /// other, but there can be an arbitrary number of them around.
     pub fn clone(&self, f: impl FnOnce(Self)) {
         f(Self {
             connect: self.connect.clone(),
         })
     }
 
+    /// Initiates a new connection with the server. Returns the client's side of the connection
+    /// initiation protocol.
     #[must_use]
-    pub fn connect(self) -> C {
-        C::fork_sync(|dual| self.connect.send(dual))
+    pub fn connect(self) -> Connect {
+        Connect::fork_sync(|dual| self.connect.send(dual))
     }
 }
 
-impl<R: Session> Connection<R> {
+impl<Resume: Session> Connection<Resume> {
+    /// Resumes the [connection](Connection), entering the server's event loop. Returns the client's
+    /// side of the connection resumption protocol.
     #[must_use]
-    pub fn resume(self) -> R {
-        R::fork_sync(|dual| (self.resume)(dual))
+    pub fn resume(self) -> Resume {
+        Resume::fork_sync(|dual| (self.resume)(dual))
     }
 }
 
